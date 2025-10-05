aeb166bbfddfb6f53ead3e6e7741eb03fc0290f2
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-05 16:38:25 +0300

    Simplify VS environment setup in CI scripts
    Removed the VS version range parameter and vswhere logic from setup-vs-env.cmd. Now directly calls the appropriate VsDevCmd.bat based on the APPVEYOR_BUILD_WORKER_IMAGE variable for Visual Studio 2015, 2017, 2019, and 2022. Updated appveyor.yml to match the new usage.

--- ci/setup-vs-env.cmd
+++ ci/setup-vs-env.cmd
@@ -1,24 +1,25 @@
 @echo off
 setlocal
 
-set VS_RANGE=%1
-
 if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
   call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" -arch=amd64
   goto :eof
 )
 
-:: For VS2017 and newer, use vswhere
-for /f "usebackq tokens=*" %%i in (`
-  "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version %VS_RANGE% -products * -requires Microsoft.Component.MSBuild -property installationPath
-`) do (
-  set VSINSTALL=%%i
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
+  goto :eof
 )
 
-if not defined VSINSTALL (
-  echo Could not find Visual Studio installation for version range %VS_RANGE%
-  exit /b 1
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
+  goto :eof
 )
 
-call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat" -arch=amd64
-endlocal
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
+  call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
+  goto :eof
+)
+
+echo Unknown image: %APPVEYOR_BUILD_WORKER_IMAGE%
+exit /b 1
