606953bfea3d58c1dced03598f31f8b074b51822
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-07 23:59:24 +0300

    PM-99 Guard std::variant usage with feature macro
    Replaced direct usage of _PM_CC_SUPPORTS_CXX17 with PM_HAS_STD_BAD_VARIANT_ACCESS to conditionally include <variant> and related code. This improves portability and ensures code only uses std::variant features when supported.

--- tests/variant/tst_variant.cpp
+++ tests/variant/tst_variant.cpp
@@ -1,17 +1,22 @@
 #include <testing.h>
 
-#include <variant>
+#define PM_HAS_STD_BAD_VARIANT_ACCESS _PM_CC_SUPPORTS_CXX17
+
+#if PM_HAS_STD_BAD_VARIANT_ACCESS
+#    include <variant>
+#endif
+#include <exception>
 
 // #include "../../variant/include/std-compat/internal/monostate_compat.h"
 
 bool test_variant()
 {
-// #ifdef _PM_CC_SUPPORTS_CXX17
-//     std::monostate ms1;
-//     std::cout << &ms1 << std::endl;
-// #endif
+    // #ifdef _PM_CC_SUPPORTS_CXX17
+    //     std::monostate ms1;
+    //     std::cout << &ms1 << std::endl;
+    // #endif
 
-#ifdef _PM_CC_SUPPORTS_CXX17
+#ifdef PM_HAS_STD_BAD_VARIANT_ACCESS
     std::bad_variant_access bva;
     std::cout << &bva << std::endl;
 #endif
