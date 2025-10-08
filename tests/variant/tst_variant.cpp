454177ef41e1c60e908f73e88d44cff438e9ebf0
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-08 06:01:36 +0300

    PM-99 Refine UCRT version checks for std::bad_variant_access

--- tests/variant/tst_variant.cpp
+++ tests/variant/tst_variant.cpp
@@ -8,25 +8,26 @@
 #    define PM_CPP_VERSION 0
 #endif
 
-// UCRT 14.44.x.x: (doesn't require c++17)
-// UCRT 14.29.x.x: (requires c++17)
-// UCRT 14.16.x.x: (requires c++17)
-// UCRT 14.0.x.x: (doesn't have c++17 support)
+// UCRT 14.44.x.x: doesn't require c++17
+// UCRT 14.29.x.x: requires c++17 (defined in <variant>)
+// UCRT 14.16.x.x: requires c++17 (defined in <variant>)
+// UCRT 14.0.x.x: doesn't have c++17 support
 #define _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_44_X_X _PM_CC_SUPPORTS_CXX17 && (PM_CRT_VERSION_MAJOR == 14) && (PM_CRT_VERSION_MINOR >= 44)
 #define _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_29_X_X PM_CPP_VERSION >= 201703L && (PM_CRT_VERSION_MAJOR == 14) && (PM_CRT_VERSION_MINOR >= 29)
 #define _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_16_X_X PM_CPP_VERSION >= 201703L && (PM_CRT_VERSION_MAJOR == 14) && (PM_CRT_VERSION_MINOR >= 16)
 #define _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_00_X_X 0
 
 // clang-format off
-#define PM_HAS_STD_BAD_VARIANT_ACCESS   _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_44_X_X || \
+#define PM_UCRT_HAS_STD_BAD_VARIANT_ACCESS   _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_44_X_X || \
                                         _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_29_X_X || \
                                         _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_16_X_X || \
                                         _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_00_X_X
+
+#define PM_HAS_STD_BAD_VARIANT_ACCESS_NOT_IN_VARIANT_HEADER _PM_HAS_STD_BAD_VARIANT_ACCESS_UCRT_14_44_X_X
 // clang-format on
 
-#if PM_HAS_STD_BAD_VARIANT_ACCESS
+#if PM_HAS_STD_BAD_VARIANT_ACCESS_NOT_IN_VARIANT_HEADER
 #    include <exception>
-// #    include <variant>
 #else
 namespace std
 {
