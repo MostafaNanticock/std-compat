41592f0cfcb174c16ef1bfcf015517d1fb1442db
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-10 17:06:04 +0300

    PM-99 Implemented mingw\4.8_std_to_string.h patch

---
+++ src/common/include/std-compat/crt_patches.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//
+// FIXME: do patching here instead of inside `detect_crt.h`
+//
+#include "internal/detect_crt.h"
--- src/common/include/std-compat/internal/mingw/4.8_std_to_string.h
+++ src/common/include/std-compat/internal/mingw/4.8_std_to_string.h
@@ -1,5 +1,14 @@
 #include <sstream>
 
+#ifdef __MINGW32_MAJOR_VERSION // Classic MinGW CRT (32-bit) is always affected
+#    define _PM_MINGW_IMPLEMENT_TO_STRING_WORKAROUND 1
+#else // Workaround needed for MinGW-w64 < 3.11
+#    if (PM_CRT_VERSION_MAJOR < 3) || (PM_CRT_VERSION_MAJOR == 3 && PM_CRT_VERSION_MINOR < 11)
+#        define _PM_MINGW_IMPLEMENT_TO_STRING_WORKAROUND 1
+#    endif
+#endif
+
+#ifdef _PM_MINGW_IMPLEMENT_TO_STRING_WORKAROUND
 namespace std
 {
 template <typename T>
@@ -11,3 +20,4 @@ std::string to_string(T value)
     return oss.str();
 }
 } // namespace std
+#endif
