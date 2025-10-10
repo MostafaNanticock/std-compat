9d609aaa535df1ee164a1e3b85436e17bd0bf955
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-10 16:44:18 +0300

    Update 4.8_std_to_string.h

--- src/common/include/std-compat/internal/mingw/4.8_std_to_string.h
+++ src/common/include/std-compat/internal/mingw/4.8_std_to_string.h
@@ -3,10 +3,11 @@
 namespace std
 {
 template <typename T>
-std::string my_to_string(T value)
+std::string to_string(T value)
 {
     std::ostringstream oss;
     oss << value;
+
     return oss.str();
 }
 } // namespace std
