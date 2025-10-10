82b57496f316fee2d205c73ff8cfd44486e07c7e
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-10 17:47:06 +0300

    Revert "PM-99 added WARN macro"
    This reverts commit 3c658d9cc1bf1ad50b3575157d1e0a6f859343ba.

--- src/common/include/std-compat/internal/crt_patches/mingw_4.8_std_to_string.h
+++ src/common/include/std-compat/internal/crt_patches/mingw_4.8_std_to_string.h
@@ -1,26 +1,5 @@
 #pragma once
 
-// Helpers to stringify __LINE__
-#define STRINGIZE_DETAIL(x) #x
-#define STRINGIZE(x) STRINGIZE_DETAIL(x)
-
-// Build "file(line): " prefix
-#define LOCATION __FILE__ "(" STRINGIZE(__LINE__) ") : "
-
-#if defined(_MSC_VER)
-// MSVC: use __pragma(message(...))
-#    define WARN(msg) __pragma(message(LOCATION msg))
-#elif defined(__GNUC__) || defined(__clang__)
-// GCC/Clang: use _Pragma with GCC warning
-#    define DO_PRAGMA(x) _Pragma(#x)
-#    define WARN(msg) DO_PRAGMA(GCC warning LOCATION msg)
-#else
-#    define WARN(msg)
-#endif
-
-// Example usage
-WARN("Hi there")
-
 #include <sstream>
 
 #ifdef __MINGW32_MAJOR_VERSION // Classic MinGW CRT (32-bit) is always affected
--- src/common/include/std-compat/internal/crt_patches/patches_mingw.h
+++ src/common/include/std-compat/internal/crt_patches/patches_mingw.h
@@ -1,26 +1,5 @@
 #pragma once
 
-// Helpers to stringify __LINE__
-#define STRINGIZE_DETAIL(x) #x
-#define STRINGIZE(x) STRINGIZE_DETAIL(x)
-
-// Build "file(line): " prefix
-#define LOCATION __FILE__ "(" STRINGIZE(__LINE__) ") : "
-
-#if defined(_MSC_VER)
-// MSVC: use __pragma(message(...))
-#    define WARN(msg) __pragma(message(LOCATION msg))
-#elif defined(__GNUC__) || defined(__clang__)
-// GCC/Clang: use _Pragma with GCC warning
-#    define DO_PRAGMA(x) _Pragma(#x)
-#    define WARN(msg) DO_PRAGMA(GCC warning LOCATION msg)
-#else
-#    define WARN(msg)
-#endif
-
-// Example usage
-WARN("Hi there")
-
 //
 // WORKAROUND for the 4.8 bug with non-existing std::to_string() implementation
 //
