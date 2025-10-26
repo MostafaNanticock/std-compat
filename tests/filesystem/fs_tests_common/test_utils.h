324e88b7a57116913438072d8f8dbb4156f113f5
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2025-10-26 22:52:35 +0300

    PM-146 fix STDC::Testing::open_stream_from_path()

--- tests/filesystem/fs_tests_common/test_utils.h
+++ tests/filesystem/fs_tests_common/test_utils.h
@@ -7,6 +7,15 @@ namespace STDC
 {
 namespace Testing
 {
+    namespace internal
+    {
+        template <typename T, typename CharT>
+        T open_stream_impl(const std::filesystem::path &path, std::ios::openmode mode, std::true_type);
+
+        template <typename T, typename CharT>
+        T open_stream_impl(const std::filesystem::path &path, std::ios::openmode mode, std::false_type);
+    } // namespace internal
+
     class FileSystemTestCase
     {
     public:
@@ -35,7 +44,24 @@ namespace Testing
     template <typename T>
     T open_stream_from_path(const std::filesystem::path &path, std::ios::openmode mode = std::ios::in)
     {
-        return T(path.c_str(), mode);
+        typedef typename T::char_type CharT;
+
+        // Narrow stream
+        return STDC::Testing::internal::open_stream_impl<T, CharT>(path, mode, typename std::is_same<CharT, char>::type());
     }
 } // namespace Testing
 } // namespace STDC
+
+// Overload for narrow streams
+template <typename T, typename CharT>
+T STDC::Testing::internal::open_stream_impl(const std::filesystem::path &path, std::ios::openmode mode, std::true_type)
+{
+    return T(path.string(), mode);
+}
+
+// Overload for wide streams
+template <typename T, typename CharT>
+T STDC::Testing::internal::open_stream_impl(const std::filesystem::path &path, std::ios::openmode mode, std::false_type)
+{
+    return T(path.wstring(), mode);
+}
